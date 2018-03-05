9670ec2fa0c67ea6ca7ac666d5eee4afdf8d55bc
s-lin <slin07@risd.edu>
2018-03-04 22:50:59 -0500

    10th commit

--- pofo/pofo_style.js
+++ pofo/pofo_style.js
@@ -14,4 +14,20 @@ $( document ).ready(function() {
 	$( ".hamburger" ).show();
 	});
 	});
+
+	var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 4500); // Change image every 4.5 seconds
+	}
+
 });
